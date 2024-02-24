f531499beb0676ed70094bce9a5d4f612fadd32e
ge <as8354093@gmail.com>
2024-02-24 13:18:55 +0800

    feat: function

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const obj = {
-    key:"hello",
-    "value":"javaScript"
+function f1(paramters) {
+    return paramters;
 }
 
+const f2 = (paramters) => {
+    return paramters;
+}
 
-document.getElementById('output').innerText = `${obj.key} ${obj.value}`;
+const f3 = function f3(paramters) {
+    return paramters;
+}
+
+document.getElementById('output1').innerText = `${f1("f1")}`;
+document.getElementById('output2').innerText = `${f2("f2")}`;
+document.getElementById('output3').innerText = `${f3("f3")}`;
